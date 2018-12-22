2eabbdae1752c47d48982a68ad244407abfe46bf
Sergey Zhulin <brainsurov@gmail.com>
2018-12-22 17:44:12 +0200

    minor updates

--- index.js
+++ index.js
@@ -5,39 +5,41 @@ module.exports = {
   'env': {
     'es6': true,
     'browser': true,
-    'node': true
+    'node': true,
   },
   'parserOptions': {
     'ecmaVersion': 7,
-    'sourceType': 'module'
+    'sourceType': 'module',
   },
   'parserOptions': {
     'ecmaFeatures': {
       'spread': true,
-      'experimentalObjectRestSpread': true
-    }
+      'experimentalObjectRestSpread': true,
+    },
   },
   'rules': {
     'fp/no-unused-expression': 1,
+    'fp/no-mutation': ['error', {'commonjs': true}],
 
     'import/prefer-default-export': 0,
+    'import/no-cycle': 0,
 
     'comma-dangle': ['error', {
       'arrays': 'always-multiline',
       'objects': 'always-multiline',
       'imports': 'always-multiline',
       'exports': 'always-multiline',
-      'functions': 'ignore'
+      'functions': 'always-multiline',
     }],
     'function-paren-newline': 0,
     'arrow-parens': ['error', 'as-needed'],
     'object-curly-spacing': ['error', 'never'],
     'object-curly-newline': 0,
-    'no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
+    'no-unused-vars': ['warn', {'argsIgnorePattern': '^_'}],
     'no-nested-ternary': 0,
     'no-unused-expressions': 0,
-    'max-len': 0,
-    'no-confusing-arrow': 0,
+    'max-len': 1,
+    'no-confusing-arrow': ['error', {allowParens: true}],
     'radix': ['error', 'as-needed'],
   },
 };
